97e63486bc14eaa352392478bed163801026a35b
Daniel Janus <dj@danieljanus.pl>
2014-02-14 10:41:31 +0000

    Add project.clj.

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject sejm "0.1"
+  :description "Polish parliament data downloader"
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [clojure-csv "2.0.1"]
+                 [enlive "1.1.5"]])
